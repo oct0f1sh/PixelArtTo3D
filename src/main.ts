f6f28d250cd70310c3c0c73be2fa83b6867947d6
Duncan MacDonald <buzzbuzz.macdonald@gmail.com>
2026-01-12 19:52:22 -0800

    Project setup

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="container">
+    <h1>Pixel Art to 3D Converter</h1>
+    <p>Project initialized. Ready for development.</p>
+  </div>
+`
